fcd8623d86059817759676f865d409b3d0d654e3
Jonathan Cogan <jonathanecogan@gmail.com>
2019-09-07 21:24:16 -0700

    cleans up diff format

--- src/diff.rs
+++ src/diff.rs
@@ -4,26 +4,27 @@ use difference::{Difference, Changeset};
 use std::io::Write;
 
 pub fn print_diff(expected: &str, actual: &str) {
+    println!("\n\nEXPECTED: ");
+    println!("{}", "┈".repeat(60));
     let Changeset { diffs, .. } = Changeset::new(expected, actual, "\n");
-
     let mut t = term::stdout().unwrap();
-
     for i in 0..diffs.len() {
         match diffs[i] {
             Difference::Same(ref x) => {
-                t.reset().unwrap();
-                writeln!(t, " {}", x);
+                t.fg(term::color::WHITE).unwrap();
+                writeln!(t, "{}", x);
             }
             Difference::Add(ref x) => {
                 t.fg(term::color::GREEN).unwrap();
-                writeln!(t, "+{}", x);
+                writeln!(t, "+{}", &x[1..]);
             }
             Difference::Rem(ref x) => {
                 t.fg(term::color::RED).unwrap();
-                writeln!(t, "-{}", x);
+                writeln!(t, "-{}", &x[1..]);
             }
         }
     }
     t.reset().unwrap();
     t.flush().unwrap();
+    println!("{}", "┈".repeat(60));
 }
